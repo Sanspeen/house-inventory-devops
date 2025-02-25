44dbef7fbcb39623fcea50f5b42da3306ccaf456
SanSpeen <ktns0930@gmail.com>
2025-02-25 12:53:09 -0500

    Implemented get method

--- src/main/java/com/house/inventory/controller/ApplianceController.java
+++ src/main/java/com/house/inventory/controller/ApplianceController.java
@@ -2,16 +2,12 @@ package com.house.inventory.controller;
 
 import com.house.inventory.model.Appliance;
 import com.house.inventory.service.ApplianceService;
-import jakarta.persistence.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/appliance")
@@ -20,12 +16,14 @@ public class ApplianceController {
     @Autowired
     private ApplianceService applianceService;
 
-
-
     @PostMapping("/create")
     public ResponseEntity<Appliance> createAppliance(@RequestBody Appliance newAppliance){
         Appliance savedAppliance = applianceService.saveAppliance(newAppliance);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedAppliance);
     }
 
+    @GetMapping("/get-all")
+    public ResponseEntity<List<Appliance>> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(applianceService.getAll());
+    }
 }
--- src/main/java/com/house/inventory/service/ApplianceService.java
+++ src/main/java/com/house/inventory/service/ApplianceService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ApplianceService {
@@ -16,6 +17,10 @@ public class ApplianceService {
     public Appliance saveAppliance(Appliance newAppliance){
         newAppliance.setBoughtDate(new Date());
         return applianceRepository.save(newAppliance);
+    }
+
+    public List<Appliance> getAll(){
+        return applianceRepository.findAll();
     }
 
 }
