2295352f7ea5811bdea2fa1b07ef671db1ef50ef
SanSpeen <ktns0930@gmail.com>
2025-03-03 02:58:41 -0500

    Added the rest of the structure for the following cruds and test deploy fails in pipleines by coverage

--- src/main/java/com/house/inventory/controller/ApplianceController.java
+++ src/main/java/com/house/inventory/controller/ApplianceController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/appliance")
@@ -25,6 +26,22 @@ public class ApplianceController {
     @GetMapping("/get-all")
     public ResponseEntity<List<Appliance>> getAll(){
         return ResponseEntity.status(HttpStatus.OK).body(applianceService.getAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Appliance> updateAppliance(@PathVariable Long id, @RequestBody Appliance appliance) {
+        return ResponseEntity.ok(applianceService.editAppliance(id, appliance));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Appliance> partiallyUpdateAppliance(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
+        return ResponseEntity.ok(applianceService.patchAppliance(id, updates));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAppliance(@PathVariable Long id) {
+        applianceService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/health-check")
---
+++ src/main/java/com/house/inventory/model/Furniture.java
@@ -0,0 +1,42 @@
+package com.house.inventory.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "furniture")
+public class Furniture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String brand;
+
+    private Date boughtDate;
+
+    @NonNull
+    private Boolean isNew;
+
+    @NonNull
+    private Double energyConsumption;
+
+    @NonNull
+    private Integer width;
+
+    @NonNull
+    private Integer length;
+}
