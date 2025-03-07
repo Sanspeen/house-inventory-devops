a1afa52b17a74d874fa678acd0ea8e52d865a28d
SanSpeen <ktns0930@gmail.com>
2025-03-06 21:43:53 -0500

    Implemented the last crud

--- src/main/java/com/house/inventory/model/Plant.java
+++ src/main/java/com/house/inventory/model/Plant.java
@@ -1,4 +1,32 @@
 package com.house.inventory.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="plant")
 public class Plant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String name;
+
+    private Date boughtDate;
+
+    @NonNull
+    private Integer estimatedLifeExpectancy; // This value is made in expected days of life.
+
+    @NonNull
+    private String environment;
+
+    @NonNull
+    private Integer currentAge; // Value in days.
 }
