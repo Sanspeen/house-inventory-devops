792feca85612e3f28ddaa8d89d1a7c0f8870e745
SanSpeen <ktns0930@gmail.com>
2025-04-07 22:15:36 -0500

    feat: upgraded v1 to v2 into the endpoints

--- src/main/java/com/house/inventory/controller/ApplianceController.java
+++ src/main/java/com/house/inventory/controller/ApplianceController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/v1/appliance")
+@RequestMapping("/v2/appliance")
 public class ApplianceController {
 
     @Autowired
