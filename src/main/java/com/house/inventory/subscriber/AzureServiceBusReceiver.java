7e85602839cbd69a3d107ca704e7746a5aad4b35
SanSpeen <ktns0930@gmail.com>
2025-04-09 01:21:39 -0500

    feat: Implemented logic to redirect to the correct ms from queue adding my models each time

--- src/main/java/com/house/inventory/subscriber/AzureServiceBusReceiver.java
+++ src/main/java/com/house/inventory/subscriber/AzureServiceBusReceiver.java
@@ -2,9 +2,16 @@ package com.house.inventory.subscriber;
 
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusProcessorClient;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.house.inventory.model.Appliance;
+import com.house.inventory.service.ApplianceService;
+import com.house.inventory.service.AzureServiceBusSender;
+import com.house.inventory.service.FurnitureService;
+import com.house.inventory.service.PlantService;
 import jakarta.annotation.PostConstruct;
+import org.aspectj.lang.annotation.SuppressAjWarnings;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +28,16 @@ public class AzureServiceBusReceiver {
 
     @Value("${azure.servicebus.subscription-name}")
     private String subscriptionName;
+
+    @Autowired
+    ApplianceService applianceService;
+    @Autowired
+    FurnitureService furnitureService;
+    @Autowired
+    PlantService plantService;
+
+    @Autowired
+    AzureServiceBusSender azureServiceBusSender;
 
     private final ObjectMapper objectMapper;
 
@@ -43,13 +60,18 @@ public class AzureServiceBusReceiver {
 
                     try {
                         Map<String, Object> messageMap = objectMapper.readValue(json, Map.class);
+                        messageMap.put("appliance", applianceService.generateGenericAppliance());
+                        messageMap.put("furniture", furnitureService.generateGenericFurniture());
+                        messageMap.put("plant", plantService.generateGenericPlant());
                         System.out.println("PROCESSED JSON: " + messageMap);
                         String msToSend = String.valueOf(messageMap.get("sendTo"));
+                        String enrichedJson = objectMapper.writeValueAsString(messageMap);
+
                         switch (msToSend){
-                            case "microservice1" -> System.out.println("REDIRECTED TO MS1");
-                            case "microservice2" -> System.out.println("REDIRECTED TO MS2");
-                            case "microservice3" -> System.out.println("REDIRECTED TO MS3");
-                            case "coordinator" -> System.out.println("REDIRECTED TO COORDINATOR");
+                            case "microservice1" -> redirectTo(enrichedJson, source, "microservice1");
+                            case "microservice2" -> redirectTo(enrichedJson, source, "microservice2");
+                            case "microservice3" -> redirectTo(enrichedJson, source, "microservice3");
+                            case "coordinator" -> redirectTo(enrichedJson, source, "coordinator");
                             default -> System.out.println("IDK THIS MS");
                         }
                     } catch (Exception e) {
@@ -63,31 +85,7 @@ public class AzureServiceBusReceiver {
         processorClient.start();
     }
 
-    @PostConstruct
-    public void startReceiverMS1() {
-        ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
-                .connectionString(connectionString)
-                .processor()
-                .topicName(topicName)
-                .subscriptionName(subscriptionName)
-                .processMessage(context -> {
-                    String json = context.getMessage().getBody().toString();
-                    String source = (String) context.getMessage().getApplicationProperties().get("source");
-                    String destination = (String) context.getMessage().getApplicationProperties().get("destination");
-
-                    System.out.printf("Message received in %s from %s to %s: %s%n", subscriptionName, source, destination, json);
-
-                    try {
-                        Map<String, Object> messageMap = objectMapper.readValue(json, Map.class);
-                        System.out.println("PROCESSED JSON: " + messageMap);
-                    } catch (Exception e) {
-                        System.err.println("Failed to parse JSON message: " + e.getMessage());
-                    }
-                })
-                .processError(context -> {
-                    System.err.println("Error in message processing: " + context.getException());
-                })
-                .buildProcessorClient();
-        processorClient.start();
+    private void redirectTo(String dynamicJsonString, String source, String destination) throws JsonProcessingException {
+        azureServiceBusSender.sendMessage(dynamicJsonString, source, destination);
     }
 }
