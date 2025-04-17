d8c7954c45332ecbc10bf198cb48d752b1d06b4d
SanSpeen <ktns0930@gmail.com>
2025-04-17 18:28:09 -0500

    feat: Code cleanance

--- src/main/java/com/house/inventory/publisher/PublishMessageController.java
+++ src/main/java/com/house/inventory/publisher/PublishMessageController.java
@@ -2,6 +2,7 @@ package com.house.inventory.publisher;
 
 import com.house.inventory.model.MessageRequest;
 import com.house.inventory.service.AzureServiceBusSender;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v2/messages")
 public class PublishMessageController {
 
+    @Autowired
     private final AzureServiceBusSender sender;
 
     public PublishMessageController(AzureServiceBusSender sender) {
         this.sender = sender;
     }
 
-    @PostMapping
+    @PostMapping("/publish")
     public ResponseEntity<String> postMessage(@RequestBody MessageRequest request,
                                               @RequestHeader("X-Source") String source,
                                               @RequestHeader("X-Destination") String destination) {
--- src/main/java/com/house/inventory/service/ApplianceService.java
+++ src/main/java/com/house/inventory/service/ApplianceService.java
@@ -1,23 +1,12 @@
 package com.house.inventory.service;
 
 import com.house.inventory.model.Appliance;
-import com.house.inventory.repository.ApplianceRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-
-import static com.fasterxml.jackson.databind.type.LogicalType.DateTime;
 
 @Service
 public class ApplianceService {
-
-    @Autowired
-    private ApplianceRepository applianceRepository;
-
     public Appliance generateGenericAppliance(){
         return Appliance.builder()
                 .isNew(true)
@@ -30,45 +19,4 @@ public class ApplianceService {
                 .build();
     }
 
-    public Appliance saveAppliance(Appliance newAppliance){
-        newAppliance.setBoughtDate(new Date());
-        return applianceRepository.save(newAppliance);
-    }
-
-    public List<Appliance> getAll(){
-        return applianceRepository.findAll();
-    }
-
-    public Appliance editAppliance(Long id, Appliance appliance){
-        return applianceRepository.findById(id).map(existingAppliance -> {
-            existingAppliance.setName(appliance.getName());
-            existingAppliance.setBrand(appliance.getBrand());
-            existingAppliance.setBoughtDate(appliance.getBoughtDate());
-            existingAppliance.setIsNew(appliance.getIsNew());
-            existingAppliance.setEnergyConsumption(appliance.getEnergyConsumption());
-            existingAppliance.setWidth(appliance.getWidth());
-            existingAppliance.setLength(appliance.getLength());
-            return applianceRepository.save(existingAppliance);
-        }).orElseThrow(() -> new NoSuchElementException("Appliance not found with id " + id));
-    }
-
-    public Appliance patchAppliance(Long id, Map<String, Object> updates){
-        return applianceRepository.findById(id).map(existingAppliance -> {
-            updates.forEach((key, value) -> {
-                switch (key) {
-                    case "name" -> existingAppliance.setName((String) value);
-                    case "brand" -> existingAppliance.setBrand((String) value);
-                    case "boughtDate" -> existingAppliance.setBoughtDate((Date) value);
-                    case "isNew" -> existingAppliance.setIsNew((Boolean) value);
-                    case "energyConsumption" -> existingAppliance.setEnergyConsumption((Double) value);
-                    case "width" -> existingAppliance.setWidth((Integer) value);
-                    case "length" -> existingAppliance.setLength((Integer) value);
-                }
-            });
-            return applianceRepository.save(existingAppliance);
-        }).orElseThrow(() -> new NoSuchElementException("Appliance not found with id " + id));
-    }
-    public void deleteById(Long id){
-        applianceRepository.deleteById(id);
-    }
 }
